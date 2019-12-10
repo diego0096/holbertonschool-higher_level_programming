621dba5f1f7372e53d1beef44827cbc89d89ea85
diego0096 <dfqz93@hotmail.com>
2019-12-10 00:12:57 +0000

    tenth commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cycle - check a linked list.
+ * @list: listint_t type
+ * Return: 1 if there is a cycle or 0 if there isn't cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp, *new;
+
+	pto1 = list;
+	pto2 = list;
+	while (pto1 != NULL && pto2 != NULL && pto2->next != NULL)
+	{
+		pto2 = pto2->next->next;
+		pto1 = pto1->next;
+		if (pto2 == pto1)
+			return (1);
+	}
+	return (0);
+}

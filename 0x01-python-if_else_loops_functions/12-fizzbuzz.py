3d236ea47d54f153a3ae17c74365f4cb69990b3c
diego0096 <dfqz93@hotmail.com>
2019-12-11 01:53:13 +0000

    eleventh commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for a in range(1, 101):
+        if (a % 3 == 0) and (a % 5 == 0):
+            a = "FizzBuzz"
+        elif a % 3 == 0:
+            a = "Fizz"
+        elif a % 5 == 0:
+            a = "Buzz"
+        print("{} ".format(a), end="")

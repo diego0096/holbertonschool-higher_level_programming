26592636bc4c86a453b6b7088c456632d5693759
diego0096 <dfqz93@hotmail.com>
2019-12-18 04:39:22 +0000

    last commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * *is_pal - function to recursively check values of head and tail in a linked list
+ * @head: double pointer of listint_t type to head of linked list
+ * @tail: single pointer of listint_t type to tail of linked list
+ * Return: 1 if palidrome or 0 if not
+ */
+int is_pal(listint_t **head, listint_t *tail)
+{
+    if (tail == NULL)
+        return (1);
+    if (is_pal(head, tail->next) && (*head)->n == tail->n)
+    {
+        *head = (*head)->next;
+        return (1);
+    }
+    return (0);
+}
+
+/**
+ * is_palindrome - function to check if singly linked list is a palidrome
+ * @head: double pointer of listint_t type to head of list
+ * Return: 0 if not palindrome, 1 if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+    if (*head == NULL || (*head)->next == NULL)
+
+    return(is_pal(head, *head));
+}

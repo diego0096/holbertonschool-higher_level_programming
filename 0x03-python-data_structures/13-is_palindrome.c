f17dc77662cbe4f3e961a02d11c763ac781f9ecb
diego0096 <dfqz93@hotmail.com>
2019-12-18 04:45:06 +0000

    last commit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * *is_pal - function to recursively check values of head and tail in a linked list
- * @head: double pointer of listint_t type to head of linked list
- * @tail: single pointer of listint_t type to tail of linked list
- * Return: 1 if palidrome or 0 if not
+ * *is_pal - function to recursively check values of head and tail.
+ * @head: double pointer of listint_t type to head.
+ * @tail: single pointer of listint_t type to tail.
+ * Return: 1 if palidrome.
  */
 int is_pal(listint_t **head, listint_t *tail)
 {
-    if (tail == NULL)
-        return (1);
-    if (is_pal(head, tail->next) && (*head)->n == tail->n)
-    {
-        *head = (*head)->next;
-        return (1);
-    }
-    return (0);
+	if (tail == NULL)
+		return (1);
+	if (is_pal(head, tail->next) && (*head)->n == tail->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
 
 /**
  * is_palindrome - function to check if singly linked list is a palidrome
- * @head: double pointer of listint_t type to head of list
+ * @head: double pointer of listint_t type to head.
  * Return: 0 if not palindrome, 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 
-    return(is_pal(head, *head));
+	return(is_pal(head, *head));
 }

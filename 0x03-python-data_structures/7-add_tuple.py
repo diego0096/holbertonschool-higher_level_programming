a2d9c57137aa40dedc25212ebe3549feb22ddb50
diego0096 <dfqz93@hotmail.com>
2019-12-18 00:35:07 +0000

    eighth commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,12 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
-    for index, i in enumerate(tuple_a):
-        if (index < 2):
-            a[index] = tuple_a[index]
-    for index, i in enumerate(tuple_b):
-        if (index < 2):
-            b[index] = tuple_b[index]
+    for i, j in enumerate(tuple_a):
+        if (i < 2):
+            a[i] = tuple_a[i]
+    for i, j in enumerate(tuple_b):
+        if (i < 2):
+            b[i] = tuple_b[i]
     sum1 = a[0] + b[0]
     sum2 = a[1] + b[1]
-    return (sum_a, sum_b)
+    return (sum1, sum2)

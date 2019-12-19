40ad22acf68cac341ddf33612bad35145d917af7
diego0096 <dfqz93@hotmail.com>
2019-12-19 18:39:49 +0000

    first commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     a = []
-    for a in my_list:
-        if a == search:
+    for n in my_list:
+        if n == search:
             a.append(replace)
         else:
-            a.append(n)
-    return new
+            a.append(a)
+    return a

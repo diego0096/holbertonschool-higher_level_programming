d63a93a39018967797be0edfc3cd658242fe674d
diego0096 <dfqz93@hotmail.com>
2019-12-19 17:00:20 +0000

    init commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     if matrix:
         new = []
         for r in matrix:
-            new.append([a ** 2 for n in r])
+            new.append([a ** 2 for a in r])
     return new

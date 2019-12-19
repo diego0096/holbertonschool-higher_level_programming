93b2b5808060943f62b5009fc34bc97ddb0f7e7a
diego0096 <dfqz93@hotmail.com>
2019-12-19 18:45:18 +0000

    first commit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    a = []
-    for n in my_list:
-        if n == search:
-            a.append(replace)
-        else:
-            a.append(a)
-    return a
+    return [replace if a == search else a for a in my_list]

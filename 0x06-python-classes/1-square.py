0bbf4fc04a71dbdededecaf9d2b9ce0bfdfc5986
diego0096 <dfqz93@hotmail.com>
2020-01-09 22:16:25 +0000

    first commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,4 @@ class Square():
         def __init__(self, size):
             '''Initialization square with size'''
 
-            '''size: The size of the square'''
             self.__size = size

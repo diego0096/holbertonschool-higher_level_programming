69bac9060ab93d63524f8a0e32fbfb681c7c9f29
diego0096 <dfqz93@hotmail.com>
2020-01-09 21:04:14 +0000

    second commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square():
+        '''Creates the square'''
+
+        def __init__(self, size):
+            '''Initialization square with size'''
+
+            '''size: The size of the square'''
+            self.__size = size

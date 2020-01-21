b7e2e1e241e92e03cd95a275995e8aca89aa38b5
diego0096 <dfqz93@hotmail.com>
2020-01-21 04:58:02 +0000

    fiked commit

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,4 +2,4 @@
 def is_kind_of_class(obj, a_class):
     '''function to check if an object is an instance or class inherited'''
 
-return isinstance(obj, a_class)
+    return (isinstance(obj, a_class))

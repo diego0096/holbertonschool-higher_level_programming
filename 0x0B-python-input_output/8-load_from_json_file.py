cd6000be7f11745dcd184a402bf279244a0c7f5a
diego0096 <dfqz93@hotmail.com>
2020-01-22 03:43:36 +0000

    eight commit

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Python function that creates an Object from a JSON file"""
+"""Function that creates an Object from a JSON file"""
 
 import json
+
 
 def load_from_json_file(filename):
     with open(filename, 'r', encoding='utf-8') as f:

46c365832a96a4a14b1e208ade7aadbdf2b1b48b
diego0096 <dfqz93@hotmail.com>
2020-01-22 04:04:28 +0000

    nineth commit

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Function that add arguments to a Python list"""
-import s
-import r
+import sys
+import os
 
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
 
 my_list = []
 
-if not r.path.exists("./add_item.json"):
+if not os.path.exists("./add_item.json"):
     save_json(my_list, "add_item.json")
 my_list = load_json("add_item.json")
 
-for items in s.argv[1:]:
+for items in sys.argv[1:]:
     my_list.append(items)
 save_json(my_list, "add_item.json")

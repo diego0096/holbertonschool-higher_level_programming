a4d6bad62d1b57627c8d66c6076ebcd82f42780c
diego0096 <dfqz93@hotmail.com>
2020-01-22 00:33:35 +0000

    first commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    '''read the content of the file'''
+
+
+    with open(filename, encoding="utf8") as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyton3
+def number_of_lines(filename=""):
+    '''return the number of lines of a text file'''
+
+
+    count = 0
+    with open(filename, encoding="utf8"=) as f:
+        for line in f:
+            count += 1
+    return count

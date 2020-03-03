256e2467f12f5cb86d8ad04aff2295889e4930b2
diego0096 <dfqz93@hotmail.com>
2020-03-03 18:03:27 +0000

    init commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+--How to show privileges of all users in mysql
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+--Create a new user in mysql and granted all priviliges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'; 

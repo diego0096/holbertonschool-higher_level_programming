82ce671102e8140dbeb58584d3e442721d84c8eb
diego0096 <dfqz93@hotmail.com>
2020-03-03 18:16:54 +0000

    fix commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- MySql Script to list all privileges for MqSql users
+-- MySql Script to list all privileges for MqSql users
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;

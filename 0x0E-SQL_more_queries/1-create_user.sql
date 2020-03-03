8dfe1753b2e53adc82c2241a693283efb97e4427
diego0096 <dfqz93@hotmail.com>
2020-03-03 18:29:12 +0000

    fix commit one

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 --Create a new user in mysql and granted all priviliges
 --Create a new user in mysql and granted all priviliges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1 '@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
 FLUSH PRIVILEGES;

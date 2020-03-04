41970302ec66bc5227eb94663b5a120cb37bd974
diego0096 <dfqz93@hotmail.com>
2020-03-04 00:56:48 +0000

    commit two

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
---create a new user and gives it only select priviliges
---create a new user and gives it only select priviliges
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates the database hbtn_0d_2 and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIDED BY 'user_0d_2_pwd'
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
-FLUSH PRIVILIGES;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
+FLUSH PRIVILEGES;

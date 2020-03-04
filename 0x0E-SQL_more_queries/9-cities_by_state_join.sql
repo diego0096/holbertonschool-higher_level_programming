9e2b6b48af2982e57b449ddbef671310eb05b4fb
diego0096 <dfqz93@hotmail.com>
2020-03-04 03:00:22 +0000

    commit nine

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 --script that list all cities
 --script that list all cities
 SELECT cities.id, cities.name, states.name
-FROM states, cities WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+FROM cities
+LEFT JOIN states
+ON cities.state_id = states.id;

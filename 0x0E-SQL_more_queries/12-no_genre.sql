1fc6d48321db904066a3fc573268cef2fe3376bf
diego0096 <dfqz93@hotmail.com>
2020-03-04 04:56:10 +0000

    last commit

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- sort table by NULL
+-- sort table by NULL
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id is NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;

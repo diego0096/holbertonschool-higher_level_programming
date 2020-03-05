a977c76533e49286a1319b2f09a76d06d187fb85
diego0096 <dfqz93@hotmail.com>
2020-03-05 04:08:42 +0000

    commit fourteen

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- script to select shows and value
+-- script to select shows and value
+SELECT name AS 'genre', COUNT(*) AS 'number_shows'
+FROM tv_genres, tv_show_genres
+WHERE tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_shows DESC;

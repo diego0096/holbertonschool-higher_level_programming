fb323cf82f348dee822aad8b82cfd8213f1fa8a3
diego0096 <dfqz93@hotmail.com>
2020-03-30 04:33:31 +0000

    commit thirteen

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sqlalchemy
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from model_state import Base, State
+
 if __name__ == "__main__":
-    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    user_name = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    eng = create_engine(connection.format(user_name, password, db_name),
-                        pool_pre_ping=True)
-    Session = sessionmaker(bind=eng)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State).filter_by(id=2).first()
-    query.name = "New Mexico"
+    item = session.query(State).filter(State.name.like('%a%'))
+    for it in item:
+        session.delete(it)
     session.commit()
+    session.close()

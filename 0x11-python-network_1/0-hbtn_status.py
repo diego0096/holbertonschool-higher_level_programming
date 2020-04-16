b19af81a4b055b64f3b58ed75fe4db01d53571fe
diego0096 <dfqz93@hotmail.com>
2020-04-16 14:32:48 +0000

    init commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,8 @@
 # Python script that fetches a url
 import urllib.request as ur
 
-with ur.urlopen('https://intranet.hbtn.io/status') as r:
-    r = r.read()
+with ur.urlopen('https://intranet.hbtn.io/status') as res:
+    res = res.read()
 
 print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(res), res))
 print('\t- utf8 content: {}'.format(str(res, 'utf-8')))

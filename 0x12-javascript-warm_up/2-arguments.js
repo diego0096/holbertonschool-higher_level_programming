db4e6ec8cab12cda4f6da271a289f10045a73c47
diego0096 <dfqz93@hotmail.com>
2020-05-05 20:08:46 +0000

    second commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// js to check arguments
+// JS to check arguments
 if (process.argv.length < 3) {
-	  console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-	  console.log('Argument found');
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  console.log('Arguments found');
 }

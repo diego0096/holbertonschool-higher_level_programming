d21aaa2a929dcc9ed3a2e2067c342cbabf63e6a7
diego0096 <dfqz93@hotmail.com>
2020-05-12 23:44:38 +0000

    second commit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response) {
-<<<<<<< HEAD
-    if (error) throw err;
-    console.log('code: ' + response.statusCode);
-=======
   if (error == null) {
     console.log('code: ' + response.statusCode);
   }
->>>>>>> 7c546f23b18497d3a2519f119412753c78834ad2
 });

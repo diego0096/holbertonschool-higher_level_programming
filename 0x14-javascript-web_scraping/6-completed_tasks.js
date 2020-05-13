ece6779c8d4d640229dc91e78fb530b48094ca7b
diego0096 <dfqz93@hotmail.com>
2020-05-13 02:46:30 +0000

    sixth commit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,22 @@
-#!usr/bin/node
+#!/usr/bin/node
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], function (error, response, body) {
-  if (error == null) {
-    const resp = {};
-    const json = JSON.parse(body);
-    for (let a = 0; a < json.lenght; a++) {
-      if (json[a].completed === true) {
-        if (resp[json[a].userId] === undefined) {
-          resp[json[a].userId] = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const result = JSON.parse(body);
+    const dic = {};
+    for (let index = 0; index < result.length; index++) {
+      if (result[index].completed === true) {
+        if (dic[result[index].userId] === undefined) {
+          dic[result[index].userId] = 1;
+        } else {
+          dic[result[index].userId] += 1;
         }
-        resp[json[a].userId]++;
       }
     }
-    console.log(resp);
+    console.log(dic);
   }
 });

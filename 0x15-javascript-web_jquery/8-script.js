e89de4a988acac492670ada8c9c6d5f5d940a1d2
diego0096 <dfqz93@hotmail.com>
2020-05-15 02:15:08 +0000

    javascript qwery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi.co/api/films/?format=json', function (data) {
+  for (let index in data.results) {
+    $('UL#list_movies').append('<li>' + data.results[index].title + '</li>');
+  }
+});
